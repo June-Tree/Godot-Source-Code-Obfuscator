4320cd746771b669e3e9c5d8dcc2ce6b57467633
Sonia C. O <108655518+June-Tree@users.noreply.github.com>
2025-09-12 08:28:42 +0100

    Update gdscript_obfuscator.py
    - The first letter of generated names cannot start with a number
    - Addition of a default name length
    - Comment processing improvement

--- Scripts/gdscript_obfuscator.py
+++ Scripts/gdscript_obfuscator.py
@@ -9,12 +9,16 @@ import time
 
 
 def obfuscate_name(length):
-    return "".join(random.choices(string.ascii_letters + string.digits, k=length)) #Generate a random obfuscated name of specified length.
+    first_char = random.choice(string.ascii_letters) # Ensure the first character is a letter
+    remaining_chars = "".join(random.choices(string.ascii_letters + string.digits, k=length-1))
+    return first_char + remaining_chars
 
-def obfuscate_gdscript(file_path, overwrite, name_length, input_path, relative_path):
+def obfuscate_gdscript(file_path, canOverwrite, name_length, input_path, relative_path):
     # Determine output file path
-    if overwrite: output_file_path = file_path
-    else: output_file_path = file_path.replace(".gd", "_obfuscated.gd")
+    if canOverwrite: output_file_path = file_path
+    else: 
+        print("Creating duplicate file...")
+        output_file_path = file_path.replace(".gd", "_obfuscated.gd")
     
     if os.path.isfile(input_path): file_name = os.path.basename(file_path)
     else: file_name = relative_path
@@ -36,22 +40,25 @@ def obfuscate_gdscript(file_path, overwrite, name_length, input_path, relative_p
     obfuscated_code = []
     empty_lines = []
 
-    #Collect data from the .gd file.
+    # Collect data from the .gd file.
     with open(file_path, 'r') as file: lines = file.readlines()
     
     for line_number, line in enumerate(lines):
-        #Process comments
-        comment_match = re.search(r'#.*$', line)
-        if comment_match:
-            comment = comment_match.group(0).strip() # Get the entire comment
-            comment_start = comment_match.start(0) # Get the start position of the comment
+        # Process comments
+        if "#" in line:
+            comment_start = line.index("#") # Get the start position of the comment
+            comment_lwhitespace = line.rfind(" ", 0, comment_start) + 1 # Get the position of the last whitespace before the comment
+            if comment_lwhitespace < comment_start: comment_start = comment_lwhitespace # Adjust comment start to include preceding whitespace if any
+            elif comment_lwhitespace == 0: comment_start = 0 # If no whitespace found, start from the beginning of the line
+            comment = line[comment_start:].rstrip() # Get the entire comment
             obfuscation_data[file_name]["comments"][comment] = {
                 "line_number": line_number+1,
                 "starting_index": comment_start
             } # Store the comment with details on its exact location
-            if comment_start == 0: empty_lines.append(line_number+1) # Include comment locations in empty lines
 
-        #Save empty line locations
+            if comment_start == 0: empty_lines.append(line_number+1) # Include empty line after comment removal in empty lines
+
+        # Save empty line locations
         if line.strip() == "":
             empty_lines.append(line_number+1)
 
@@ -130,55 +137,66 @@ def main():
     input_path = input_path.replace("'", "") # Remove any single quotes
     input_path = input_path.replace('"', "") # Remove any double quotes
 
-    overwrite = input("Do you want to overwrite the file provided? (yes/no): ").strip().lower() == "yes"
+    canOverwrite = input("Do you want to overwrite the file provided? (yes/no): ").strip().lower() == "yes"
     
-    name_length = int(input("Enter the desired length for generated names: "))
-    while not isinstance(name_length, int) or name_length <= 0: #Check if name_length is a positive number
-        print("Please enter a valid positive integer for name length.")
-        name_length = int(input("Enter the desired length for generated names: "))
+    name_length = input("Enter the desired length for generated names: ")
+    if not isinstance(name_length, int) or name_length <= 0: # Check if name length is a positive number
+        print("Since a valid positive integer was not provided, the default length of 5 will be used.")
+        name_length = 5
+
+    mapping_file = None
 
     all_obfuscation_data = {}
     # Check if the input is a directory or a file
     if os.path.isfile(input_path) and input_path.endswith(".gd") and not input_path.endswith("_obfuscated.gd"):
         print(f"Obfuscating \"{input_path}\"...")
+        print("...")
         mapping_file = input_path.replace(".gd", "_obfuscation_data.json")
-        obfuscation_data = obfuscate_gdscript(input_path, overwrite, name_length, input_path, os.path.basename(input_path))
+        obfuscation_data = obfuscate_gdscript(input_path, canOverwrite, name_length, input_path, os.path.basename(input_path))
         all_obfuscation_data.update(obfuscation_data)
+        if not canOverwrite: print("Duplicate file created")
         print(f"Code obfuscation successful. Mapping file created.")
     elif os.path.isdir(input_path):
         folder_path = os.path.abspath(input_path)
         folder_name = os.path.basename(folder_path)
         folder_parent_dir = os.path.dirname(folder_path)
         mapping_file = folder_path + "_obfuscation_data.json"
-        #Duplicate and rename the the original folder
-        if not overwrite:
+        # Duplicate and rename the the original folder
+        if not canOverwrite:
+            print("Duplicating folder...")
             # Create the new folder path for obfuscation
             obfuscated_folder_name = os.path.basename(folder_path) + "_obfuscated"
             obfuscated_folder_path = os.path.join(folder_parent_dir, obfuscated_folder_name)
 
             # Copy the source folder to the new folder path and obfuscate the copy
             if os.path.exists(obfuscated_folder_path):
-                overwrite_ob = input(f"There is already a folder called \"{obfuscated_folder_name}\". Do you want to overwrite it? (yes/no): ").strip().lower() == "yes"
-                if overwrite_ob:
+                canOverwrite_ob = input(f"There is already a folder called \"{obfuscated_folder_name}\". Do you want to overwrite it? (yes/no): ").strip().lower() == "yes"
+                if canOverwrite_ob:
                     shutil.rmtree(obfuscated_folder_path)
                     shutil.copytree(input_path, obfuscated_folder_path)
-                    print(f"Obfuscating source code in \"{folder_name}\" as \"{obfuscated_folder_name}\"...")
+                    print(f"\"{obfuscated_folder_name}\" overwritten.")
+                    print(f"Obfuscating source code in \"{obfuscated_folder_name}\"...")
                 else:
                     print("Terminating operation...")
+                    print("...")
                     time.sleep(1)
                     print("Operation terminated.")
                     return
-            else: shutil.copytree(input_path, obfuscated_folder_path)
+            else: 
+                shutil.copytree(input_path, obfuscated_folder_path)
+                print("Duplicate folder created.")
+                print(f"Obfuscating source code in \"{folder_name}\" as \"{obfuscated_folder_name}\"...")
+
             input_path = obfuscated_folder_path
-            overwrite = True
+            canOverwrite = True
         
         print(f"Creating code map...")
         # Walk through the directory
         for folder, subfolderNames, files in os.walk(input_path):
             # Remove repository data
             if ".git" in subfolderNames:
-                remove_vc = input(f"Would you like to remove version control? (yes/no): ").strip().lower() == "yes"
-                if remove_vc:
+                canRemoveVC = input(f"Would you like to remove version control? (yes/no): ").strip().lower() == "yes"
+                if canRemoveVC:
                     git_folder = os.path.join(folder, ".git")
                     # Forcefully delete the .git directory using system command
                     if os.name == "nt": # For Windows
@@ -193,14 +211,17 @@ def main():
                     objective_file_path = file_path.replace("_obfuscated", "")
                     relative_path = os.path.relpath(objective_file_path, mapping_file)[3:]
                     print(f"Obfuscating \"{file}\"...")
-                    obfuscation_data = obfuscate_gdscript(file_path, overwrite, name_length, input_path, relative_path)
+                    print("...")
+                    obfuscation_data = obfuscate_gdscript(file_path, canOverwrite, name_length, input_path, relative_path)
                     all_obfuscation_data.update(obfuscation_data)
                     print(f"Obfuscation successful. Mapping details updated.")
         print(f"Code obfuscation successful.")
     else:
         return print("The specified path is not valid. Please enter a valid directory or a .gd file.")
     if mapping_file: 
-        with open(mapping_file, 'w') as json_file: json.dump(all_obfuscation_data, json_file, indent=4)
+        with open(mapping_file, 'w') as json_file:
+            json.dump(all_obfuscation_data, json_file, indent=4)
+            print(f"Mapping file created at \"{mapping_file}\".")
 
 if __name__ == '__main__':
     print("GDScript Code Obfuscator")
